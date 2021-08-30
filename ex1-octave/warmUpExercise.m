da6cc81513c1ce69288e1c174785d108603bacd1
S010MON <leon@bdebnath.com>
2021-08-30 11:11:03 +0200

    test

--- ex1-octave/warmUpExercise.m
+++ ex1-octave/warmUpExercise.m
@@ -6,8 +6,7 @@ A = [];
 % ============= YOUR CODE HERE ==============
 % Instructions: Return the 5x5 identity matrix 
 %               In octave, we return values by defining which variables
-%               represent the return values (at the top of the file)
-%               and then set them accordingly. 
+%               represent the return values (at the top of the file). 
 
 A = eye(5);
 
